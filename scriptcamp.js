991fae2f8f8458e61a3b2d2332c0d0df8c8f1571
nmul1801 <nicholas.m.mulligan@gmail.com>
2023-03-07 16:51:12 -0500

    fixed img hover buggy

--- scriptcamp.js
+++ scriptcamp.js
@@ -1,12 +1,20 @@
 const cells = Array.from(document.querySelectorAll(".table-cell"));
 var blurbs = document.querySelectorAll(".blurb");
 
-for (let i = 0; i < cells.length; i++) {
-    cells[i].onmouseover = function(){
-        console.log("fading in ")
-        $("#blurb" + (i + 1)).fadeIn(400);
-    };
-    cells[i].onmouseleave = function(){
-        $("#blurb" + (i + 1)).fadeOut(400);
-    };
-}
+const healthy_img_paths = ["url('nick.jpg')", "url('nick.jpg')", "url('nick.jpg')", "url('nick.jpg')"]
+const sick_img_paths = ["url('nicksick.jpg')", "url('nicksick.jpg')", "url('nicksick.jpg')", "url('nicksick.jpg')"]
+
+$( document ).ready(function() {
+    for (let i = 0; i < cells.length; i++) {
+        cells[i].style.backgroundImage =  "url('nick.jpg')";
+        cells[i].onmouseover = function(){
+            console.log("fading in ")
+            $("#blurb" + (i + 1)).fadeIn(300);
+            cells[i].style.backgroundImage = sick_img_paths[i];
+        };
+        cells[i].onmouseleave = function(){
+            $("#blurb" + (i + 1)).fadeOut(300);
+            cells[i].style.backgroundImage = healthy_img_paths[i];
+        };
+    }
+})
